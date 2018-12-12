46ec561a4b1e7a99082ff62f923b958860cbf336
Carlo Emilio Montanari <carlidel@ymail.com>
2018-12-12 11:44:34 +0100

    Add more control printing for inverted logic

--- arduino_code/linefollow.ino
+++ arduino_code/linefollow.ino
@@ -1,3 +1,10 @@
+void print_sensori_ir(int * sensori_ir){
+    for(int i = 0; i < 5; i++){
+      Serial.print(sensori_ir[i]);
+    }
+    Serial.print("\n");
+  }
+
 void initirsensors(){
   pinMode(IRsensor0pin, INPUT);
   pinMode(IRsensor1pin, INPUT);
@@ -13,12 +20,15 @@ void measure_ir(){
   sensori_ir[3]=digitalRead(IRsensor3pin);
   sensori_ir[4]=digitalRead(IRsensor4pin);
 
+  print_sensori_ir(sensori_ir);
+
   if(invert_ir){
+    Serial.println("Inverted Logic:");
     for(int i = 0; i < 5; i++){
-      sensori_ir[i] = (sensori_ir[i] == HIGH ? LOW : HIGH);
+      sensori_ir[i] = ((sensori_ir[i] == HIGH) ? LOW : HIGH);
       }
+    print_sensori_ir(sensori_ir);
     }
-  //Serial.println(sensori_ir);
 }
 
 //follow_line
@@ -27,33 +37,40 @@ void follow_line(){
   if(sensori_ir[2]==HIGH){ //sensore centrale su linea nera
     if(sensori_ir[1]==LOW && sensori_ir[3]==LOW){ //sensori adiacenti su bianco
       avanti();  //finchè la barca va
+      Serial.println("Avanti!");
       setspeed(linefollow_speed,linefollow_speed);
     }
     else if(sensori_ir[1]==HIGH && sensori_ir[3]==LOW){ //sensore dx su linea nera
        sinistra();  //sinistra
+       Serial.println("Sinistra!");
        setspeed(0,linefollow_speed);
     }
     else if(sensori_ir[1]==LOW && sensori_ir[3]==HIGH){  //sensore sx su linea nera
       destra();  //gira a destra
+      Serial.println("Destra!");
       setspeed(linefollow_speed,0);
     }
   }
   else if(sensori_ir[2]==LOW){  //sensore centrale fuori linea
     if(sensori_ir[1]==HIGH && sensori_ir[3]==LOW){ //sensore 
-      sinistra();  
+      sinistra();
+      Serial.println("Sinistra!");  
       setspeed(0,linefollow_speed);
     }
     else if(sensori_ir[1]==LOW && sensori_ir[3]==HIGH){ 
        destra();
+       Serial.println("Destra!");
        setspeed(linefollow_speed,0);
     }
     //controllo sensori periferici
     else if(sensori_ir[0]==HIGH && sensori_ir[4]==LOW){
       sinistra();
+      Serial.println("Sinistra!");
       setspeed(0, linefollow_speed);
     }
     else if(sensori_ir[0] == LOW && sensori_ir[4]==HIGH){
       destra();
+      Serial.println("Destra!");
       setspeed(linefollow_speed, 0);
     }
   }
